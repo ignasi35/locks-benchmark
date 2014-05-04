739e0523b767f59d803f1aa0e03ed661de0e8a53
Pierre Laporte <pierre@pingtimeout.fr>
2014-05-04 11:00:39 +0200

    Added results of run with consumed CPU factor = 128

---
+++ results/generate-graphs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE=$1
+
+cd $DATE
+mkdir -p results/images/
+
+../generate-data-file.sh 24 > results/benchmark-results.data
+../generate-data-file.sh 24 | tr ' ' ';' > results/benchmark-results.csv
+
+cd results/
+
+for graphtype in dirty volatile synchronized reentrant-read-write-lock atomic adder stamped-lock reads writes
+do
+  gnuplot ../../$graphtype.gnuplot > images/$graphtype.png
+done
+
+
